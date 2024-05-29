c14a86a261390a8c246c371cc975d6e36ee4138b
Mostafijur Rahman <mostafijurrahman163@gmail.com>
2024-05-29 16:55:37 +0600

    fix sizes

--- client/src/components/BasicViewer.jsx
+++ client/src/components/BasicViewer.jsx
@@ -1,51 +1,92 @@
-import { useRef } from "react";
+import { forwardRef, useRef } from "react";
 import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
 
-const responsiveSizes = [400, 800, 1200, 2500];
+const maxResolution = 2500;
 
 export default function BasicViewer() {
+  const maxScale = Math.round(maxResolution / window.innerWidth);
+
   const imgRef = useRef();
-  function onTransform() {}
+  const scaleRef = useRef({
+    scale: 1,
+    positionX: 0,
+    positionY: 0,
+  });
+
+  function onTransformEnd() {
+    const deviceWidth = window.innerWidth;
+    const scaledWidth = deviceWidth * scaleRef.current.scale;
+    const scaledVW = (100 / deviceWidth) * scaledWidth;
+
+    const urlFragments = imgRef.current.currentSrc.split("/");
+    const imgName = urlFragments[urlFragments.length - 1];
+    const nameFragments = imgName.split("_");
+    const sizeFragments = nameFragments[nameFragments.length - 1].split(".");
+
+    const currentSrcWidth = isNaN(Number(sizeFragments[0]))
+      ? imgRef.current.naturalWidth
+      : Number(sizeFragments[0]);
+
+    console.log("curr src width", currentSrcWidth);
+    console.log("scaled vw", scaledVW);
+    console.log(
+      "pixel ratio",
+      currentSrcWidth / scaledWidth,
+      window.devicePixelRatio
+    );
+
+    if (currentSrcWidth / scaledWidth < window.devicePixelRatio) {
+      imgRef.current.sizes = scaledVW + "vw";
+    }
+  }
 
   return (
     <TransformWrapper
       initialScale={1}
-      //   initialPositionX={0}
-      //   initialPositionY={110}
-      //   minPositionX={200}
-      //   minPositionY={400}
-      //   onZoom={(ref, e) => console.log("on zoom: ", e)}
-      //   onWheelStop={(ref, e) => console.log("on wheel", e)}
-      onTransformed={(_, e) => {
-        const deviceWidth = window.innerWidth;
-        const scaledWidth = deviceWidth * e.scale;
-
-        if (800 / scaledWidth < window.devicePixelRatio) {
-          const nextImgSize = responsiveSizes.find(
-            (size) => size / scaledWidth >= window.devicePixelRatio
-          );
-
-          if (imgRef.current) {
-            imgRef.current.src = `/../../public/images/cell_${nextImgSize}.webp`;
-          }
-        }
+      maxScale={maxScale}
+      onTransformed={(_, state) => {
+        scaleRef.current = state;
       }}
+      onZoomStop={onTransformEnd}
     >
-      {({ zoomIn, zoomOut, resetTransform, ...rest }) => (
-        <TransformComponent>
-          <img
-            ref={imgRef}
-            src="/../../public/images/cell_800.webp"
-            alt="Basic zoom"
-            // sizes="100vw"
-            // srcSet="
-            //     ./../../public/images/cell_400.webp 400w,
-            //     ./../../public/images/cell_800.webp 800w,
-            //     ./../../public/images/cell_1200.webp 1200w,
-            //     ./../../public/images/cell_2500.webp 2500w"
-          />
-        </TransformComponent>
-      )}
+      <ImgComponent ref={imgRef} />
     </TransformWrapper>
   );
 }
+
+const ImgComponent = forwardRef(function (_, ref) {
+  //   const imgRef = useRef();
+
+  //   useImperativeHandle(
+  //     ref,
+  //     () => {
+
+  //       console.log("img src: ", currentSrcWidth);
+
+  //       return {
+  //         currentSrcWidth: currentSrcWidth,
+  //         setSizes(sizes) {
+  //           if (isNaN(sizes) && sizes <= 0) return;
+  //           imgRef.current.sizes = sizes + "vw";
+  //         },
+  //       };
+  //     },
+  //     []
+  //   );
+
+  return (
+    <TransformComponent>
+      <img
+        ref={ref}
+        src="/../../public/images/cell.jpg"
+        alt="Basic zoom"
+        sizes="100vw"
+        srcSet="
+                ./../../public/images/cell_400.webp 400w,
+                ./../../public/images/cell_800.webp 800w,
+                ./../../public/images/cell_1200.webp 1200w,
+                ./../../public/images/cell_2500.webp 2500w"
+      />
+    </TransformComponent>
+  );
+});
